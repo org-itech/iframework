55f0b298439eb9d153461294cc3753f768ec07c9
刘强 <Gavineil@outlook.com>
2019-05-20 17:15:42 +0800

    init

---
+++ iframework-domain/src/main/java/org/itech/iframework/domain/data/OptionSet.java
@@ -0,0 +1,12 @@
+package org.itech.iframework.domain.data;
+
+import java.util.HashSet;
+
+/**
+ * OptionSet
+ *
+ * @param <O>
+ * @author liuqiang
+ */
+public class OptionSet<O extends OptionItem> extends HashSet<O> {
+}
